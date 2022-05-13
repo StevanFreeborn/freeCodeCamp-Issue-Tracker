d9febeaf3176c0fe0ff8afdb756f43fad3acc9dc
StevanFreeborn <sfreeborn7@gmail.com>
2022-05-13 09:02:59 -0500

    finish writing tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -25,18 +25,23 @@ suite('Functional Tests', () => {
     const openFilter = { open: true };
     const statusFilter = { status_text: 'development' };
 
+    const updateIssueText = 'issue text has been updated.';
+    const updateAssignedTo = 'Steve';
+
+    const invalidId = '6275d73f52abd03df630186e';
+
     test('POST /api/issues/{project} create an issue with every field', (done) => {
 
         chai.request(server)
-        .post(`/api/issues/${project}`)
+        .post(`/api/issues/${ project }`)
         .set('content-type', 'application/x-www-form-urlencoded')
-        .type('type')
-        .send(`issue_title=${issue.issue_title}`)
-        .send(`issue_text=${issue.issue_text}`)
-        .send(`created_by=${issue.created_by}`)
-        .send(`assigned_to=${issue.assigned_to}`)
-        .send(`open=${issue.open}`)
-        .send(`status_text=${issue.status_text}`)
+        .type('form')
+        .send(`issue_title=${ issue.issue_title }`)
+        .send(`issue_text=${ issue.issue_text }`)
+        .send(`created_by=${ issue.created_by }`)
+        .send(`assigned_to=${ issue.assigned_to }`)
+        .send(`open=${ issue.open }`)
+        .send(`status_text=${ issue.status_text }`)
         .end((err, res) => {
 
             if (err) console.log(err);
@@ -55,12 +60,12 @@ suite('Functional Tests', () => {
     test('POST /api/issues/{project} create an issue with required fields', (done) => {
 
         chai.request(server)
-        .post(`/api/issues/${project}`)
+        .post(`/api/issues/${ project }`)
         .set('content-type', 'application/x-www-form-urlencoded')
-        .type('type')
-        .send(`issue_title=${issue.issue_title}`)
-        .send(`issue_text=${issue.issue_text}`)
-        .send(`created_by=${issue.created_by}`)
+        .type('form')
+        .send(`issue_title=${ issue.issue_title }`)
+        .send(`issue_text=${ issue.issue_text }`)
+        .send(`created_by=${ issue.created_by }`)
         .end((err, res) => {
 
             if (err) console.log(err);
@@ -77,12 +82,12 @@ suite('Functional Tests', () => {
     test('POST /api/issues/{project} create an issue without required fields', (done) => {
 
         chai.request(server)
-        .post(`/api/issues/${project}`)
+        .post(`/api/issues/${ project }`)
         .set('content-type', 'application/x-www-form-urlencoded')
-        .type('type')
-        .send(`issue_title=${issue.assigned_to}`)
-        .send(`issue_title=${issue.open}`)
-        .send(`issue_title=${issue.status_text}`)
+        .type('form')
+        .send(`issue_title=${ issue.assigned_to }`)
+        .send(`issue_title=${ issue.open }`)
+        .send(`issue_title=${ issue.status_text }`)
         .end((err, res) => {
 
             if (err) console.log(err);
@@ -99,7 +104,7 @@ suite('Functional Tests', () => {
     test('GET /api/issues/{project} view a projects issues', (done) => {
 
         chai.request(server)
-        .get(`/api/issues/${project}`)
+        .get(`/api/issues/${ project }`)
         .end((err, res) => {
 
             if (err) console.log(err);
@@ -116,7 +121,7 @@ suite('Functional Tests', () => {
     test('GET /api/issues/{project}?open=true view a projects open issues', (done) => {
 
         chai.request(server)
-        .get(`/api/issues/${project}`)
+        .get(`/api/issues/${ project }`)
         .query(openFilter)
         .end((err, res) => {
 
@@ -134,7 +139,7 @@ suite('Functional Tests', () => {
     test('GET /api/issues/{project}?open=true&status=qa view a projects open issues in development', (done) => {
 
         chai.request(server)
-        .get(`/api/issues/${project}`)
+        .get(`/api/issues/${ project }`)
         .query(openFilter)
         .query(statusFilter)
         .end((err, res) => {
@@ -152,57 +157,223 @@ suite('Functional Tests', () => {
 
     test('PUT /api/issues/{project} update one field on an issue', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .post(`/api/issues/${ project }`)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(`issue_title=${ issue.issue_title }`)
+        .send(`issue_text=${ issue.issue_text }`)
+        .send(`created_by=${ issue.created_by }`)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            chai.request(server)
+            .put('/api/issues/{project}')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .type('form')
+            .send(`_id=${ res.body._id }`)
+            .send(`issue_text=${ updateIssueText }`)
+            .end((err, res) => {
+
+                if (err) console.log(err);
+
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+
+                done();
+
+            });
+
+
+        });
 
     });
 
     test('PUT /api/issues/{project} update multiple fieds on an issue', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .post(`/api/issues/${ project }`)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(`issue_title=${ issue.issue_title }`)
+        .send(`issue_text=${ issue.issue_text }`)
+        .send(`created_by=${ issue.created_by }`)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            chai.request(server)
+            .put('/api/issues/{project}')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .type('form')
+            .send(`_id=${ res.body._id }`)
+            .send(`issue_text=${ updateIssueText }`)
+            .send(`assigned_to=${ updateAssignedTo }`)
+            .end((err, res) => {
+
+                if (err) console.log(err);
+
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+
+                done();
+
+            });
+
+
+        });
 
     });
 
     test('PUT /api/issues/{project} update an issue without _id', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .put('/api/issues/{project}')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(`issue_text=${ updateIssueText }`)
+        .send(`issue_text=${ updateAssignedTo }`)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            assert.equal(res.status, 200);
+            assert.equal(res.type, 'application/json');
+
+            done();
+
+        });
 
     });
 
     test('PUT /api/issues/{project} update an issue without any fields', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .post(`/api/issues/${ project }`)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(`issue_title=${ issue.issue_title }`)
+        .send(`issue_text=${ issue.issue_text }`)
+        .send(`created_by=${ issue.created_by }`)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            chai.request(server)
+            .put('/api/issues/{project}')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .type('form')
+            .send(`_id=${ res.body._id }`)
+            .end((err, res) => {
+
+                if (err) console.log(err);
+
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+
+                done();
+
+            });
+
+
+        });
 
     });
 
     test('PUT /api/issues/{project} update an issue with invalid _id', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .put('/api/issues/{project}')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(`_id=${ invalidId }`)
+        .send(`issue_text=${ updateIssueText }`)
+        .send(`issue_text=${ updateAssignedTo }`)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            assert.equal(res.status, 200);
+            assert.equal(res.type, 'application/json');
+
+            done();
+
+        });
 
     });
 
     test('DELETE /api/issues/{project} delete an issue', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .post(`/api/issues/${ project }`)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(`issue_title=${ issue.issue_title }`)
+        .send(`issue_text=${ issue.issue_text }`)
+        .send(`created_by=${ issue.created_by }`)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            chai.request(server)
+            .delete('/api/issues/{project}')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .type('form')
+            .send(`_id=${ res.body._id }`)
+            .end((err, res) => {
+
+                if (err) console.log(err);
+
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+
+                done();
+
+            });
+
+
+        });
 
     });
 
     test('DELETE /api/issues/{project} delete an issue with invalid _id', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .delete('/api/issues/{project}')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(`_id=${ invalidId }`)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            assert.equal(res.status, 200);
+            assert.equal(res.type, 'application/json');
+
+            done();
+
+        });
 
     });
 
     test('DELETE /api/issues/{project} delete an issue with missing _id', (done) => {
 
-        assert.fail();
-        done();
+        chai.request(server)
+        .delete('/api/issues/{project}')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send(``)
+        .end((err, res) => {
+
+            if (err) console.log(err);
+
+            assert.equal(res.status, 200);
+            assert.equal(res.type, 'application/json');
+
+            done();
+
+        });
 
     });
 
